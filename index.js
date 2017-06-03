cfb27c13a859f9c5798381e6655dfb14e2ece779
Pablo López // plopesc <plopesc@gmail.com>
2017-06-03 11:17:22 +0200

    Example of fs module usage

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var requestHandlers = require('./requestHandlers');
 
 var handle = {};
 handle['/'] = requestHandlers.start;
+handle['/show'] = requestHandlers.show;
 handle['/start'] = requestHandlers.start;
 handle['/upload'] = requestHandlers.upload;
 
