3734ba85a3c69f78109cca01ef2cb3c850d20ea4
Pablo López // plopesc <plopesc@gmail.com>
2017-06-03 09:30:43 +0200

    Single thread demonstration

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require('http');
 
+var try_count = 0;
+
 var onRequest = function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
+  response.write('Hello World ' + ++try_count);
   response.end();
 };
 
 http.createServer(onRequest).listen(8888);
+console.log ('start');
