d0e5594cea5eef228c4eff3f4b6ddfd2c10cd6d8
Pablo López // plopesc <plopesc@gmail.com>
2017-06-03 09:41:02 +0200

    Convert server.js into a node module

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var http = require('http');
 
-var try_count = 0;
+function start() {
+  var onRequest = function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  };
 
-var onRequest = function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World ' + ++try_count);
-  response.end();
-};
+  http.createServer(onRequest).listen(8888);
+  console.log('start');
+}
 
-http.createServer(onRequest).listen(8888);
-console.log ('start');
+exports.start = start;
