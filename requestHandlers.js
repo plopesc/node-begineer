ba53c19ea69a6d310629c995c77ee34f6adf51e8
Pablo López // plopesc <plopesc@gmail.com>
2017-06-03 11:30:50 +0200

    Final example. Upload image form and redirect to page to view it

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 var querystring = require("querystring"),
-  fs = require("fs");
+  fs = require("fs"),
+  formidable = require("formidable");
 
-function start(response, postData) {
+function start(response, request) {
   console.log("Request handler 'start' was called.");
   var body = '<html>'+
     '<head>'+
@@ -9,9 +10,9 @@ function start(response, postData) {
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Submit file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -21,14 +22,29 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('You have sent: ' + querystring.parse(postData).text);
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  console.log('About to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+    /* Possible error on Windows systems:
+     tried to rename to an already existing file */
+    fs.rename(files.upload.path, '/tmp/test.jpg', function(err) {
+      if (err) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.jpg');
+      }
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('received image:<br/>');
+    response.write('<img src="/show" />');
+    response.end();
+  });
 }
 
-function show(response, postData) {
+function show(response, request) {
   console.log("Request handler 'show' was called.");
   fs.readFile('/tmp/test.jpg', 'binary', function(error, file) {
     if(error) {
